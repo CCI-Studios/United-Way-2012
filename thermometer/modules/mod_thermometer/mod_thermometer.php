07a7487a0c656107a2b618997b4cc889bc389c01
Jonathan Bennett <jbennett@ccistudios.com>
2012-09-06 16:44:17 -0400

    Base thermometer style

--- thermometer/modules/mod_thermometer/mod_thermometer.php
+++ thermometer/modules/mod_thermometer/mod_thermometer.php
@@ -1,16 +1,24 @@
-<?php defined('_JEXEC') or die; ?>
+<?php
+defined('_JEXEC') or die;
+JFactory::getDocument()->addStyleSheet('/modules/mod_thermometer/mod_thermometer.css');
+?>
 
-<? print_r($params); ?>
-
-<div class="thermometer">
-	<h2><?= $params->get('title1') ?></h2>
-</div>
-
-<div class="thermometer">
-	<h2><?= $params->get('title2') ?></h2>
-	<div class="thermo">
+<? for ($tm = 1; $tm <= 2; $tm ++): ?>
+	<div class="mod_thermometer">
+		<h2><?= $params->get("name$tm") ?></h2>
+		<p>Current $<?= number_format($params->get("current$tm"), 0, '.', ','); ?></p>
+		<div class="thermo">
+			<div class="asdf">
+				<ul>
+					<li>Goal $<?= number_format($params->get("goal$tm"), 0, '.', ',') ?></li>
+					<? for($i = 7; $i >= 1; $i--): ?>
+						<li>$<?= number_format($i * $params->get("goal$tm")/8.0, 0, '.', ',') ?></li>
+					<? endfor; ?>
+					<li>0</li>
+				</ul>
+				<div class="mercury" style="height: <?= $params->get("current$tm")/$params->get("goal$tm")*100 ?>%;"></div>
+			</div>
+			<div class="ball"></div>
+		</div>
 	</div>
-	<div class="labels">
-	</div>
-	<div class="ball"></div>
-</div>
+<? endfor; ?>
