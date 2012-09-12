a33b19c630facf9090a7c9631b9457241ee7f4d0
Matt Elliott <melliott@ccistudios.com>
2012-09-12 09:04:39 -0400

    favicon link

--- template/templates/unitedway/index.php
+++ template/templates/unitedway/index.php
@@ -36,7 +36,7 @@ $typekit = 'ubu2hmw';
  	<jdoc:include type="head" />
 
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-	<link rel="shortcut icon" href="/templates/<?= $this->template ?>/resources/favicon.ico">
+	<link rel="icon" type="image/x-icon" href="/templates/<?= $this->template ?>/resources/favicon.ico">
 	<link rel="apple-touch-icon" href="/templates/<?= $this->template ?>/resources/apple-touch-icon.png">
 
 	<!-- load css -->
