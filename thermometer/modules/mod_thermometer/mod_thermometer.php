464f939f6af70cff8225c9e6693e2ea0301bb94b
Matt Elliott <melliott@ccistudios.com>
2012-09-12 14:35:11 -0400

    changed layout of thermometer

--- thermometer/modules/mod_thermometer/mod_thermometer.php
+++ thermometer/modules/mod_thermometer/mod_thermometer.php
@@ -2,7 +2,7 @@
 defined('_JEXEC') or die;
 JFactory::getDocument()->addStyleSheet('/modules/mod_thermometer/mod_thermometer.css');
 ?>
-
+<div class="custom">
 <? for ($tm = 1; $tm <= 2; $tm ++): ?>
 	<div class="mod_thermometer">
 		<h2><?= $params->get("name$tm") ?></h2>
@@ -10,15 +10,15 @@ JFactory::getDocument()->addStyleSheet('/modules/mod_thermometer/mod_thermometer
 		<div class="thermo">
 			<div class="asdf">
 				<ul>
-					<li>Goal $<?= number_format($params->get("goal$tm"), 0, '.', ',') ?></li>
+					<li>Goal: $<?= number_format($params->get("goal$tm"), 0, '.', ',') ?></li>
 					<? for($i = 7; $i >= 1; $i--): ?>
 						<li>$<?= number_format($i * $params->get("goal$tm")/8.0, 0, '.', ',') ?></li>
 					<? endfor; ?>
 					<li>0</li>
 				</ul>
-				<div class="mercury" style="height: <?= $params->get("current$tm")/$params->get("goal$tm")*100 ?>%;"></div>
+				<div class="mercury" style="height: <?= $params->get("current$tm")/$params->get("goal$tm")*100 ?>%;"><div></div></div>
 			</div>
-			<div class="ball"></div>
 		</div>
 	</div>
 <? endfor; ?>
+</div>
